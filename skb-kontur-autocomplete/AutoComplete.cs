396c83d4417374e0f697baf7e35539e04594a79f
Alex Skiffin <alex@skiffin.ru>
2015-07-16 22:30:34 +0500

    first step

---
+++ skb-kontur-autocomplete/AutoComplete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace skb_kontur_autocomplete
+{
+    public class AutoComplete
+    {
+        public AutoComplete()
+        {
+        }
+        public AutoComplete(string path)
+        {
+        }
+
+        public bool GetVariant(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- skb-kontur-autocomplete/Program.cs
+++ skb-kontur-autocomplete/Program.cs
@@ -5,10 +5,21 @@ using System.Text;
 
 namespace skb_kontur_autocomplete
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoComplete complete;
+            if (args.Length > 0)
+                complete = new AutoComplete(args[1]);
+            else
+                complete = new AutoComplete();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                Console.WriteLine(complete.GetVariant(input));
+            }
+        }
+    }
 }
